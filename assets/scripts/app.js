dd96a7b3346cc0949b986f6bd258ba26aa75edef
Peder Andreas Nielsen <peder1976@gmail.com>
2015-03-18 16:53:10 +0100

    gulp setup

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,17 @@
 (function () {
-        define(['jquery'], function ($) {
-            "use strict";
-            var App;
+    define(['jquery'], function ($) {
+        "use strict";
+        var App;
 
-            App = function () {
-                return {
-                    init: function () {
-                        console.log('Hello App');
-                    }
-                };
+        App = function () {
+            return {
+                init: function () {
+                    console.log('Hello App!');
+                }
             };
+        };
 
-            return App;
+        return App;
 
-        });
+    });
 })();
