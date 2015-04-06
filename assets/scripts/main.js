7b9ff623258cf8517b0b5a2c3813a4c81a27496f
Peder Andreas Nielsen <peder1976@gmail.com>
2015-04-06 17:24:07 +0200

    relative js path in main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
 require.config({
-    baseUrl: '/wp-content/themes/ripples/assets/scripts',
+    baseUrl: './wp-content/themes/ripples/assets/scripts',
     paths: {
         'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min' //ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min
     }
