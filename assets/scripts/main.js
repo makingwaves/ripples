c08c9c1c118d324c51459a06662913532c210f6d
Peder Andreas Nielsen <peder1976@gmail.com>
2015-05-02 08:37:15 +0200

    rjs main.js hardcoded baseUrl

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,26 +1,10 @@
-require.config((function () {
-    var ripplesFolder = './wp-content/themes/ripples',
-        jsFolder,
-        bowerUrl;
-    try {
-        ripplesFolder = php_vars.themeURI;
-    } catch (e) {}
-    bowerUrl = ripplesFolder + '/bower_components/';
-    jsFolder = ripplesFolder + '/assets/scripts';
-    return {
-        baseUrl: jsFolder,
-        paths: {
-            'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min'/*,
-            'jquery.sidr' : bowerUrl + 'sidr/jquery.sidr.min'*/
-        },
-        shim: {
-            'jquery.sidr': {
-                deps: ['jquery'],
-                exports: 'jquery.sidr'
-            }
-        }
-    };
-})());
+require.config({
+    baseUrl: '/example.dev/wp-content/themes/ripples/assets/scripts',
+    paths: {
+        'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min'
+    }
+
+});
 
 require(['app'], function (App) {
     new App().init();
