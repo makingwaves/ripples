1d9c002d5d8a7ddb27ca963a4450670ba31d8776
Peder Andreas Nielsen <peder1976@gmail.com>
2015-06-03 13:02:50 +0200

    center container layout, acf options page

--- lib/acf.php
+++ lib/acf.php
@@ -31,5 +31,24 @@ function my_acf_json_load_point( $paths ) {
 
 //option page : http://www.advancedcustomfields.com/resources/options-page/
 /*if( function_exists('acf_add_options_page') ) {
-	acf_add_options_page();
+	acf_add_options_page(array(
+		'page_title' 	=> __('Theme General Settings', 'ripples'),
+		'menu_title'	=> __('Theme Settings', 'ripples'),
+		'menu_slug' 	=> __('theme-general-settings', 'ripples'),
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
 }*/
+
+function acf_set_featured_image( $value, $post_id, $field ) {
+	delete_post_thumbnail( $post_id );
+	if ( $value != '' ) {
+		//Add the value which is the image ID to the _thumbnail_id meta data for the current post
+		add_post_meta( $post_id, '_thumbnail_id', $value );
+	}
+
+	return $value;
+}
+
+// acf/update_value/name={$field_name} - filter for a specific field based on it's name
+//add_filter( 'acf/update_value/name=post_thumbnail', __NAMESPACE__ . '\\acf_set_featured_image', 10, 3 );
