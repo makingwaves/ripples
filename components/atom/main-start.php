c52ee58c2e59b80ac3f3bb420a65be9e0f90b31f
Peder Andreas Nielsen <peder1976@gmail.com>
2015-06-04 10:11:18 +0200

    tabindex -1 on main name

--- components/atom/main-start.php
+++ components/atom/main-start.php
@@ -1,3 +1,3 @@
 <?php $itemtype = isset($value->itemtype) ? $value->itemtype : 'http://schema.org/Blog'; ?>
-<a name="main"></a>
+<a name="main" tabindex="-1"></a>
 <main id="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="<?php echo $itemtype; ?>">
