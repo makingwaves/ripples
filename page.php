3ddf9772e4cb517e32d84804d177deb53f12a05c
Peder Andreas Nielsen <peder1976@gmail.com>
2016-01-27 13:57:20 +0100

    removed a comment

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php inc( 'atom', 'main-start' ); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<?php inc('template_part', 'content-page', null, basename(get_page_template_slug(), '.php'));  //standard page, includes components/template_part/content-page[-template] with no flexible layout ?>
+	<?php inc('template_part', 'content-page', null, basename(get_page_template_slug(), '.php')); ?>
 <?php endwhile; endif; ?>
 <?php inc( 'atom', 'main-end' ); ?>
