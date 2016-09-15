4e99d67783decaa7bfa43181d73117d279001e88
Peder Andreas Nielsen <peder1976@gmail.com>
2016-09-15 13:41:13 +0200

    new favicon tags

--- components/atom/head.php
+++ components/atom/head.php
@@ -8,8 +8,7 @@
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.png">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.ico" type="image/x-icon" />
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.png" />
 	<?php wp_head(); ?>
-
 </head>
